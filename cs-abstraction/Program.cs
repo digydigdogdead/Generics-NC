c2a80b3910ca59e7b34687da600ffd23c574865f
Katherine Hurst <katherine.hurst@northcoders.com>
2024-10-21 15:22:51 +0100

    add a Program class and main console

---
+++ cs-abstraction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_abstraction
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("The 'cs-abstraction' project is now running!");
+        }
+    }
+}
