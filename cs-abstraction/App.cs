a07f5a5b57b7e9304f8b7dfd38b8944ea93ab9e2
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-11 14:50:36 +0000

    Added App class and constructor

---
+++ cs-abstraction/App.cs
@@ -0,0 +1,24 @@
+﻿using cs_abs_enc_sprint.abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_abstraction
+{
+    internal class App
+    {
+        public string Name { get; set; }
+        private ApplicationFunctions Function { get; set; }
+        public bool RequiresConnection { get; set; }
+        public Device Device { get; set; }
+
+        public App(string name, ApplicationFunctions function, bool requiresConnection)
+        {
+            Name = name;
+            Function = function;
+            RequiresConnection = requiresConnection;
+        }
+    }
+}
---
+++ cs-encapsulation/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_encapsulation
+{
+    internal class Task
+    {
+        private string Name
+        {
+            get; set;
+        }
+
+        private bool IsCompleted { get; set; }
+    } 
+}
