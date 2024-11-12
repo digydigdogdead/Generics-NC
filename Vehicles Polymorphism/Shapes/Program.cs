9b162238937eb24b1aa7b008d077af31d6c205af
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-12 16:10:41 +0000

    Added a Octagon shape class

--- Vehicles Polymorphism/Shapes/Program.cs
+++ Vehicles Polymorphism/Shapes/Program.cs
@@ -4,56 +4,62 @@
     {
         static void Main(string[] args)
         {
-            List<Shape> shapes = new List<Shape>{
-                new Circle(5),
-                new Rectangle(2, 5),
-                new Triangle(3, 7)
-            };
+            //List<Shape> shapes = new List<Shape>{
+            //    new Circle(5),
+            //    new Rectangle(2, 5),
+            //    new Triangle(3, 7)
+            //};
 
-            foreach (Shape shape in shapes)
-            {
-                Console.WriteLine(shape.GetType().Name + " area = " + shape.CalculateArea());
-            }
+            //foreach (Shape shape in shapes)
+            //{
+            //    Console.WriteLine(shape.GetType().Name + " area = " + shape.CalculateArea());
+            //}
 
-            List<Shape3D> shapes3d = new List<Shape3D>();
+            //List<Shape3D> shapes3d = new List<Shape3D>();
 
-            foreach (Shape shape in shapes)
-            {
-                if (shape is Circle circle)
-                {
-                    shapes3d.Add(new Sphere(circle));
-                    shapes3d.Add(new Cylinder(circle, 5));
-                    shapes3d.Add(new Cone(circle, 5));
-                }
-                else if (shape is Triangle triangle)
-                {
+            //foreach (Shape shape in shapes)
+            //{
+            //    if (shape is Circle circle)
+            //    {
+            //        shapes3d.Add(new Sphere(circle));
+            //        shapes3d.Add(new Cylinder(circle, 5));
+            //        shapes3d.Add(new Cone(circle, 5));
+            //    }
+            //    else if (shape is Triangle triangle)
+            //    {
 
-                    shapes3d.Add(new Prism(triangle, 5));
-                    shapes3d.Add(new Pyramid(triangle, 5));
-                }
-            }
-            foreach (Shape3D shape3d in shapes3d)
-            {
-                Console.WriteLine(shape3d.GetType().Name + " with " +
-                shape3d.BaseShape.GetType().Name + " base | Volume = " +
-                shape3d.CalculateVolume());
-            }
+            //        shapes3d.Add(new Prism(triangle, 5));
+            //        shapes3d.Add(new Pyramid(triangle, 5));
+            //    }
+            //}
+            //foreach (Shape3D shape3d in shapes3d)
+            //{
+            //    Console.WriteLine(shape3d.GetType().Name + " with " +
+            //    shape3d.BaseShape.GetType().Name + " base | Volume = " +
+            //    shape3d.CalculateVolume());
+            //}
 
-            Prism regularPrism = new Prism(new Triangle(2, 2), 2);
+            //Prism regularPrism = new Prism(new Triangle(2, 2), 2);
 
-            Prism circularPrism = new Prism(new Circle(2), 2);
+            //Prism circularPrism = new Prism(new Circle(2), 2);
 
-            double regularPrismVolume = regularPrism.CalculateVolume();
-            Console.WriteLine(regularPrismVolume);
+            //double regularPrismVolume = regularPrism.CalculateVolume();
+            //Console.WriteLine(regularPrismVolume);
 
-            double circularPrismVolume = circularPrism.CalculateVolume();
-            Console.WriteLine(circularPrismVolume);
+            //double circularPrismVolume = circularPrism.CalculateVolume();
+            //Console.WriteLine(circularPrismVolume);
 
-            Prism rectangularPrism = new Prism(new Rectangle(2, 2), 2);
-            double rectangularPrismVolume = rectangularPrism.CalculateVolume();
-            Console.WriteLine(rectangularPrismVolume);
+            //Prism rectangularPrism = new Prism(new Rectangle(2, 2), 2);
+            //double rectangularPrismVolume = rectangularPrism.CalculateVolume();
+            //Console.WriteLine(rectangularPrismVolume);
 
-            Console.WriteLine(circularPrism.CalculateVolume());
+            //Console.WriteLine(circularPrism.CalculateVolume());
+
+            RegularOctagon myOctagon = new RegularOctagon(2);
+            Console.WriteLine(myOctagon.CalculateArea());
+
+            Prism octagonalPrism = new Prism(myOctagon, 2);
+            Console.WriteLine(octagonalPrism.CalculateVolume());
         }
     }
 }
