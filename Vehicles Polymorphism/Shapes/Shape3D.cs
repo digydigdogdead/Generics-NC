127a1658a22a06da6f7a169b9eab7eb2c2e1af22
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-13 17:10:37 +0000

    Added a Golden Ratio constant to the Shape3D base class

--- Vehicles Polymorphism/Shapes/Shape3D.cs
+++ Vehicles Polymorphism/Shapes/Shape3D.cs
@@ -8,6 +8,7 @@ namespace Shapes
 {
     public abstract class Shape3D
     {
+        public double GoldenRatio = 1.61803398874989484820458683436;
         public Shape BaseShape { get; set; }
         public abstract double CalculateVolume();
     }
